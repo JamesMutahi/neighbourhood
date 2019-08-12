d5caaa17c830d2ae1e7f206670050aa9928b78af
JamesMutahi <mutahijames0@gmail.com>
2019-08-12 09:53:24 +0300

    Add forms for neighbourhood

---
+++ neighbours/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile, Neighbourhood, Business, Post
+
+
+class HoodForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Name'].widget = forms.TextInput()
+        self.fields['police'].widget = forms.TextInput()
+        self.fields['health'].widget = forms.TextInput()
+
+    class Meta:
+        model = Neighbourhood
+        exclude = ('admin',)
