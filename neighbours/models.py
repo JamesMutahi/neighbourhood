3a9ca5182653a89aa4abf3f00ba83ffabc04ab8c
JamesMutahi <mutahijames0@gmail.com>
2019-08-12 09:42:14 +0300

    Add neighbourhood model class

--- neighbours/models.py
+++ neighbours/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from users.models import Profile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    Name = models.TextField()
+    display = models.ImageField(upload_to='groups/', default='groups/group.png')
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(default='Random group')
+    police = models.TextField(default="999")
+    health = models.TextField(default="213")
+
+    def __str__(self):
+        return self.Name
+
